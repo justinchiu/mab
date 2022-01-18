85eec293add310759989fec767c2ad2ab27ce756
Justin Chiu <justinchiu@g.harvard.edu>
2022-01-18 17:33:30 -0500

    dialogue

--- dialogue.py
+++ dialogue.py
@@ -1,3 +1,6 @@
+
+from typing import Optional
+
 import numpy as np
 import pomdp_py
 
@@ -35,7 +38,7 @@ planner = pomdp_py.POMCP(
     #rollout_policy = problems[0].agent.policy_model, # need to change per agent?
 )
 
-def plan(planner, problem, steps_left):
+def plan(planner, problem, steps_left) -> pomdp_py.Action:
     planner.clear_agent()
     planner.set_max_depth(steps_left)
     planner.set_rollout_policy(problem.agent.policy_model)
@@ -44,120 +47,134 @@ def plan(planner, problem, steps_left):
 
 # communicate by passing boolean functions that apply to the dot you mention
 
-def observe(action_A, attrs_A, attrs_B):
+def observe(action_A, attrs_A, attrs_B) -> np.array:
     if not isinstance(action_A, Ask):
         return False
     # whether player B can see anything with the attributes of player A's ask
-    feedback = np.zeros_like(action_A.val)
+    # give back to player A
+    response_from_B = np.zeros_like(action_A.val)
+    # the ask gives information about dots A has
+    # use to update player B's belief
+    observation_for_B = np.zeros_like(action_A.val)
     for i, (attr, present) in enumerate(zip(attrs_A, action_A.val)):
-        feedback[i] = present and attr in attrs_B
-    return feedback
-
-def map_obs(action_A, attrs_A, attrs_B):
-    if not isinstance(action_A, Ask):
-        return False
-    # whether player B can see anything with the attributes of player A's ask
-    observation = np.zeros_like(action_A.val)
+        response_from_B[i] = present and attr in attrs_B
     for i, attr in enumerate(attrs_B):
         if attr in attrs_A:
-            observation[i] = action_A.val[attrs_A.index(attr)]
-    return observation
+            observation_for_B[i] = action_A.val[attrs_A.index(attr)]
+    return response_from_B, observation_for_B
 
 """
 On the first turn, player A and B must initialize their belief trees.
 Turns then proceed following
 
 Player A                    Player B
-Respond (player B first action = Pass)
-Update belief
+Update belief from response_from_B
+Respond to ask_B (player B first action = Pass)
+Update belief from ask_B
 Plan
 Ask
-combine response + ask
-                            Respond
+                            Update belief from response_from_A
+                            Respond 
                             Update belief if Asked about present dot
                             Plan
                             Ask
-                            combine response + ask
+
+Simplification: Responses are sent separately from Asks
 """
+
+def take_turn(
+    id_A,
+    turn,
+    action_A,
+    response_from_B,
+    action_B,
+    attrs_A, attrs_B,
+    num_dots,
+    max_turns,
+):
+    # We are player A
+    if response_from_B is not None and isinstance(action_A, Ask):
+        # Process response from B to previous action_A
+        belief_update(
+            problems[id_A].agent, action_A, response_from_B,
+            robot_state_A, countdown_state,
+            planner,
+        )
+
+    if isinstance(action_B, Ask):
+        # Respond if asked, only after first turn
+        response_from_A, observation_for_A_vec = observe(action_B, attrs_B, attrs_A)
+        response_from_A = ProductObservation({
+            id: 1 if action_B.val[id] and response_from_A[id] else 0
+        for id in range(num_dots)})
+
+        # convert Ask from B to an Observation for A
+        observation_for_A = ProductObservation({
+            id: 1 if observation_for_A_vec[id] else 0
+        for id in range(num_dots)})
+
+        if observation_for_A_vec.sum() > 0:
+            # create dummy action
+            action_A0 = Ask(observation_for_A)
+            belief_update(
+                problems[0].agent, action_A0, observation_for_A,
+                robot_state_A, countdown_state,
+                planner,
+            )
+    elif isinstance(action_B, Select):
+        response_from_A = None
+        # must select
+        pass
+    else:
+        response_from_A = None
+
+    # Plan and ask
+    action_A = plan(planner, problems[id_A], steps_left = max_turns - turn)
+    reward_A = problems[id_A].env.state_transition(action_A, execute=True)
+
+    return response_from_A, action_A, reward_A
+
 
 # initialize trees for both agents
 # not sure this is necessary
 _ = plan(planner, problems[0], steps_left = max_turns)
 _ = plan(planner, problems[1], steps_left = max_turns)
 
+action_A = Pass()
 action_B = Pass()
+response_from_A = None
+response_from_B = None
+default_response = ProductObservation({id: 0 for id in range(num_dots)})
 for turn in range(max_turns):
     # player A goes first
-    if isinstance(action_B, Ask):
-        # Respond if asked, only after first turn
-        response_from_A = observe(action_B, attrs_B, attrs_A)
-        response_to_B = ProductObservation({
-            id: 1 if action_B.val[id] and response_from_A[id] else 0
-        for id in range(num_dots)})
-
-        # convert Ask from B to an Observation for A
-        observation_for_A_vec = map_obs(action_B, attrs_B, attrs_A)
-        observation_for_A = ProductObservation({
-            id: 1 if observation_for_A_vec[id] else 0
-        for id in range(num_dots)})
-
-        if observation_for_A_vec.sum() > 0:
-            action_B0 = Ask(obseration_for_A)
-            belief_update(
-                problems[0].agent, Pass(), observation_for_A,
-                robot_state, countdown_state,
-                planner,
-            )
-    elif isinstance(action_B, Select):
-        # must select
-        pass
-
-    # Plan and ask
-    action_A = plan(planner, problems[0], steps_left = max_turns - turn)
-    reward_A = problems[0].env.state_transition(action_A, execute=True)
+    response_from_A, action_A, reward_A = take_turn(
+        id_A = 0,
+        turn = turn,
+        action_A = action_A,
+        response_from_B = response_from_B,
+        action_B = action_B,
+        attrs_A = attrs_A, attrs_B = attrs_B,
+        num_dots = num_dots,
+        max_turns = max_turns
+    )
 
     print(f"Turn {turn}")
     if isinstance(action_B, Ask):
         print(f"Response A: {response_from_A}")
     print(f"Action A: {action_A}")
 
-    if isinstance(action_A, Ask):
-        # player B observes action_A and just responds (for now)
-        response_from_B = observe(action_A, attrs_A, attrs_B)
-        observation_for_A = ProductObservation({
-            id: 1 if action_A.val[id] and response_from_B[id] else 0
-        for id in range(num_dots)})
-        # double check response
-        real_observation = problems[0].env.provide_observation(
-            problems[0].agent.observation_model, action_A)
-
-        # convert Ask from A to Observation for B
-        observation_for_B_vec = map_obs(action_A, attrs_A, attrs_B)
-        observation_for_B = ProductObservation({
-            id: 1 if observation_for_B_vec[id] else 0
-        for id in range(num_dots)})
-        if observation_for_B_vec.sum() > 0:
-            # only update belief if gained information
-            # call plan to initialize tree for player B
-            _ = plan(planner, problems[1], steps_left = max_turns - turn)
-            action_B0 = Ask(observation_for_B)
-            belief_update(
-                problems[1].agent, action_B0, observation_for_B,
-                robot_state, countdown_state,
-                planner,
-            )
-    elif isinstance(action_A, Select):
-        # must select
-        pass
-
-    # player B can also send more information
-    action_B = plan(planner, problems[1], steps_left = max_turns - turn)
-    reward_B = problems[1].env.state_transition(action_B, execute=True)
-
+    response_from_B, action_B, reward_B = take_turn(
+        id_A = 1,
+        turn = turn,
+        action_A = action_B,
+        response_from_B = response_from_A,
+        action_B = action_A,
+        attrs_A = attrs_B, attrs_B = attrs_A,
+        num_dots = num_dots,
+        max_turns = max_turns,
+    )
     if isinstance(action_A, Ask):
         print(f"Response B: {response_from_B}")
     print(f"Action B: {action_B}")
 
     import pdb; pdb.set_trace()
-
-
